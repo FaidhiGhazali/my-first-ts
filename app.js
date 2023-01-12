69e834e844f2fff9eddc253352abd80b890fdfdd
Faidhi Ghazali <faidhighazali@Faidhis-MacBook-Pro.local>
2023-01-12 15:32:13 +0800

    unknown type tutorial

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-//----Function return type & void
-function add(num1, num2, num3) {
-    return num1 + num2 + num3;
+//---unknown type
+var userInput;
+var userValue;
+userInput = 5;
+userInput = "max";
+if (typeof userInput === 'string') {
+    userValue = userInput;
 }
-function printResult(num) {
-    console.log("The sum is " + num);
-}
-printResult(add(1, 2, 3));
-//-----Function as type
-var combinedValues;
-combinedValues = add;
-console.log(combinedValues(2, 2, 2));
-//-----Function as callback
-function addAndhandle(n1, n2, callback) {
-    var result = n1 + n2;
-    callback(result);
-}
-addAndhandle(10, 20, (function (result) {
-    console.log(result);
-}));
---
+++ app.ts
@@ -0,0 +1,10 @@
+//---unknown type
+let userInput: unknown
+let userValue: string 
+
+userInput = 5
+userInput = "max"
+
+if (typeof userInput === 'string') {
+    userValue = userInput
+}
